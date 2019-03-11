a24230bfb9093d79eab09e77753e954385fd0636
Ryan Szeto <szetor@umich.edu>
2019-03-10 20:05:48 -0400

    Upgraded to Flask

--- src/server.py
+++ src/server.py
@@ -1,9 +1,11 @@
-import numpy as np
+import json
 import os
 import sys
-import json
-from bottle import Bottle, run, template, request, static_file, abort
-# from caffenet import CaffeNet, load_image_from_bytestream
+import traceback
+
+import numpy as np
+from flask import Flask, render_template, request
+
 from utils import load_config, load_image_from_bytestream
 
 # Filter allowed extensions server-side
@@ -13,37 +15,24 @@ ALLOWED_EXTS = ['.png', '.jpg', '.jpeg']
 config = load_config('config.json')
 HOSTNAME = config['HOSTNAME']
 PORT = config['PORT']
-DEPLOY_PROTOTXT_PATH = config['DEPLOY_PROTOTXT_PATH']
-WEIGHTS_PATH = config['WEIGHTS_PATH']
-
-# Initialize Caffe wrapper
-# caffe_net = CaffeNet(DEPLOY_PROTOTXT_PATH, WEIGHTS_PATH)
 
 # Use dummy CaffeNet
-class DummyCaffeNet:
-
-    def __init__(self, caffe_model_path, deploy_protoxt_path):
-        pass
+class DummyCaffeNet(object):
 
     def predict(self, image):
         p = np.random.rand()
         return [p, 1-p]
 
-caffe_net = DummyCaffeNet(DEPLOY_PROTOTXT_PATH, WEIGHTS_PATH)
+caffe_net = DummyCaffeNet()
 
-app = Bottle()
+app = Flask(__name__)
 
 @app.route('/')
 def sendIndex():
-    return static_file('index.html', root='')
-
-# Serve static files
-@app.route('/<filename:path>')
-def send_static(filename):
-    return static_file(filename, root='')
+    return render_template('index.html')
 
 # Process uploaded image. The request should have a file in the 'image' attribute.
-@app.route('/upload', method='POST')
+@app.route('/upload', methods=['POST'])
 def submitImage():
     imageData = request.files.get('image')
     filename, ext = os.path.splitext(imageData.filename)
@@ -51,7 +40,7 @@ def submitImage():
         # Throw error
         abort(400, 'File type not supported')
     else:
-        image = load_image_from_bytestream(imageData.file)
+        image = load_image_from_bytestream(imageData.stream)
         result = caffe_net.predict(image)
 
         ret = {
@@ -59,15 +48,15 @@ def submitImage():
         }
         return json.dumps(ret)
 
-@app.error(400)
-@app.error(500)
+@app.errorhandler(400)
+@app.errorhandler(500)
 def handleError(error):
-    ret = {
-        '_status_code': error._status_code,
-        '_status_line': error._status_line,
-        'body': error.body,
-        'traceback': error.traceback
-    }
-    return json.dumps(ret)
+    stack_trace = traceback.format_exc()
+    error_message = error.message
 
-run(app, host=HOSTNAME, port=PORT)
+    ret = {
+        'body': stack_trace,
+        'traceback': stack_trace
+    }
+
+    return json.dumps(ret)
