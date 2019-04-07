0906de0773fcc7eb41cd53850a585bd2bab63892
Ryan Szeto <szetor@umich.edu>
2019-04-07 12:41:32 -0400

    Download images with multiple threads

--- pytorch/collect_images.py
+++ pytorch/collect_images.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 import sys
+from queue import Queue
+from threading import Lock, Thread
 from urllib.request import urlretrieve
 
 import requests
@@ -10,20 +12,60 @@ from imgurpython import ImgurClient
 ALLOWED_EXTS = ['.png', '.jpg', '.jpeg']
 
 
-def download_list(url_dest_pairs):
-    num_downloads = 0
-    for url, dest in url_dest_pairs:
-        print('Downloading %s to %s' % (url, dest))
-        try:
-            urlretrieve(url, dest)
-        except:
-            print("Unexpected error:", sys.exec_info()[0])
-            continue
-        num_downloads += 1
-    return num_downloads
+class StopWorkToken:
+    pass
 
 
-def download_not_anime_images(client_id, client_secret, num_pages, subreddits):
+def download_list(url_dest_pairs, num_workers):
+    q = Queue()
+    num_downloads_counter = [0]
+    num_downloads_lock = Lock()
+
+    def download_list_worker():
+        while True:
+            # Get item (either (url, dest) or StopWorkToken)
+            item = q.get()
+            # Kill thread if StopWorkToken is received
+            if isinstance(item, StopWorkToken):
+                break
+            # Try to download from the URL
+            print('Downloading %s to %s' % item)
+            try:
+                urlretrieve(*item)
+            except Exception as e:
+                print(e)
+                continue
+            # Update counter
+            with num_downloads_lock:
+                num_downloads_counter[0] += 1
+            # Decrement task count
+            q.task_done()
+
+    # Create threads
+    threads = []
+    for _ in range(num_workers):
+        t = Thread(target=download_list_worker)
+        t.start()
+        threads.append(t)
+
+    # Put jobs into queue
+    for pair in url_dest_pairs:
+        q.put(pair)
+
+    # Wait for tasks to finish
+    q.join()
+
+    # Put in a StopWorkToken for each worker
+    for _ in range(num_workers):
+        q.put(StopWorkToken())
+    # Wait for all workers to terminate (i.e. each one receives a StopWorkToken)
+    for t in threads:
+        t.join()
+
+    return num_downloads_counter[0]
+
+
+def download_not_anime_images(client_id, client_secret, num_pages, subreddits, num_workers):
 
     client = ImgurClient(client_id, client_secret)
     not_anime_dir = os.path.join('data', 'images', 'not_anime')
@@ -44,11 +86,11 @@ def download_not_anime_images(client_id, client_secret, num_pages, subreddits):
     url_dest_pairs = [(url, (os.path.join(not_anime_dir, os.path.basename(url)))) for url in urls]
 
     # Download the images
-    num_downloads = download_list(url_dest_pairs)
+    num_downloads = download_list(url_dest_pairs, num_workers)
     print('Downloaded %d images over %d gallery pages x %d galleries' % (num_downloads, num_pages, len(subreddits)))
 
 
-def download_anime_images(num_pages):
+def download_anime_images(num_pages, num_workers):
 
     anime_dir = os.path.join('data', 'images', 'anime')
     if not os.path.exists(anime_dir):
@@ -70,7 +112,7 @@ def download_anime_images(num_pages):
                 dest = os.path.join(anime_dir, '%08d%s' % (id, ext))
                 url_dest_pairs.append((url, dest))
 
-    num_downloads = download_list(url_dest_pairs)
+    num_downloads = download_list(url_dest_pairs, num_workers)
     print('Downloaded %d images over %d pages' % (num_downloads, num_pages))
 
 
@@ -85,13 +127,15 @@ if __name__ == '__main__':
     parser_not_anime.add_argument('--subreddits', type=str, nargs='+', default=[
         'art', 'comics', 'comicbooks', 'mapporn', 'marvel', 'dccomics'
     ])
+    parser_not_anime.add_argument('--num_workers', type=int, default=4, help='Number of download threads/workers')
 
     parser_anime = subparsers.add_parser('anime')
-    parser_anime = parser_anime.add_argument('--num_pages', type=int, default=1)
+    parser_anime.add_argument('--num_pages', type=int, default=1)
+    parser_anime.add_argument('--num_workers', type=int, default=4, help='Number of download threads/workers')
 
     args = parser.parse_args()
 
     if args.cmd == 'notanime':
-        download_not_anime_images(args.client_id, args.client_secret, args.num_pages, args.subreddits)
+        download_not_anime_images(args.client_id, args.client_secret, args.num_pages, args.subreddits, args.num_workers)
     else:
-        download_anime_images(args.num_pages)
+        download_anime_images(args.num_pages, args.num_workers)
